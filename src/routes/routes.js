d3671c400fecbf36e11941bc983da96d7924499e
Dixit Parmar <Dparmar006@users.noreply.github.com>
2022-01-13 21:53:26 +0530

    Bookmark mock page added

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,3 +1,4 @@
+import Bookmarks from '../apps/bookmarks/Bookmarks'
 import Dashboard from '../apps/dashboard/Dashboard'
 import Notes from '../apps/notes/Notes'
 import BasicLayout from '../layouts/BasicLayout'
@@ -16,6 +17,14 @@ export const publicRoutes = [
     component: (
       <BasicLayout title='Notes'>
         <Notes />
+      </BasicLayout>
+    )
+  },
+  {
+    pathname: '/bookmarks',
+    component: (
+      <BasicLayout title='Bookmarks'>
+        <Bookmarks />
       </BasicLayout>
     )
   }
